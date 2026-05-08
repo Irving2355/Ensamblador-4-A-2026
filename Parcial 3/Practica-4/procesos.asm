dfaeda1d59d39cfeb346fd76ea88d73aa5f9b7ce
Irving2355 <it235597@gmail.com>
2026-05-08 09:28:15 -0600

    Clase 08-05-2026

--- Parcial 3/Practica-4/procesos.asm
+++ Parcial 3/Practica-4/procesos.asm
@@ -4,6 +4,88 @@ default rel
 global contar_aprobados, mejor_indice, subir_puntos
 section .text
 
-%define EDAD_OFFSET 32
+%define EDAD_OFFSET         32
 %define CALIFICACION_OFFSET 36
-%define TAM_ALUMNO 40 
+%define TAM_ALUMNO          40 
+
+contar_aprobados:
+    xor eax, eax 
+
+    cmp edx, 0
+    jle .fin_contar
+
+    .loop_contar:
+    mov r10d, [rcx + CALIFICACION_OFFSET]
+
+    cmp r10d, r8d
+    jl .no_aprobado
+
+    inc eax
+
+    .no_aprobado:
+    add rcx, TAM_ALUMNO
+    dec edx 
+    jnz .loop_contar
+
+    .fin_contar:
+    ret 
+
+mejor_indice:
+    cmp edx, 0
+    jg .hay_alumnos
+
+    mov eax, -1
+    ret 
+
+    .hay_alumnos:
+    mov r10d, [rcx + CALIFICACION_OFFSET]
+    xor eax, eax
+
+    ;indice actual
+    mov r9d, 1
+
+    add rcx, TAM_ALUMNO
+    dec edx 
+
+    jz .fin_mejor
+
+    .loop_mejor:
+    mov r11d, [rcx + CALIFICACION_OFFSET]
+    
+    cmp r11d, r10d
+    jle .no_es_mejor
+
+    mov r10d, r11d
+    mov eax, r9d
+
+    .no_es_mejor:
+    add rcx, TAM_ALUMNO
+    inc r9d
+
+    dec edx
+    jnz .loop_mejor
+    .fin_mejor:
+    ret
+
+subir_puntos:
+    cmp edx, 0
+    jle .fin_subir
+
+    .loop_subir:
+    mov eax, [rcx + CALIFICACION_OFFSET]
+
+    add eax, r8d
+
+    cmp eax, 100
+    jle .guardad_cal
+
+    mov eax, 100
+
+    .guardad_cal:
+    mov [rcx + CALIFICACION_OFFSET], eax
+
+    add rcx, TAM_ALUMNO
+    dec edx
+    jnz .loop_subir
+    .fin_subir:
+    ret 
